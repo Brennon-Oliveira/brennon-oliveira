0ae821673a29da0c18bf85573b0243a5aa02d75a
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2021-05-04 20:42:37 -0300

    Services concluido

--- src/components/Home/Services.jsx
+++ src/components/Home/Services.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Service from './../Service';
 import style from './../../styles/components/home/Services.module.css';
 
 export default function Services(){
+
+    const [services, setServices] = useState([
+        'Landing Pages para anunciar o seu negócio',
+        'Site de vendas para o seu negócio',
+        'Páginas promocionais para divulgar seu trabalho',
+        'Sites pessoais para você que sempre desejou',
+        'Sistema de estoque e armazenamento',
+        'E-Mail Marketing para fidelizar seus clientes',
+        'Google ads e outras formas de alavancar o seu site',
+    ])
+
     return(
-        <div></div>
+        <section id={style.services}>
+            <div className="container">
+                <h2>Serviços</h2>
+                <div className={style.servicesContainer}>
+                    {
+                        services.map((val, i)=>{
+                            if(i == services.length-1){
+                                return <Service text={val} id={i+1} last={true}/>
+                            } else {
+                                return <Service text={val} id={i+1} last={false}/>;
+                            }
+                        })
+
+                    }
+                    
+                </div>
+            </div>
+        </section>
     )
 }
