daaa358e4ca1cf59cc04e93a61bda9b09dfef180
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2021-05-05 00:23:45 -0300

    menu mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import react from 'react';
 import { Link, Element} from 'react-scroll';
 import style from './../styles/components/Header.module.css';
+import SlideToggle from "react-slide-toggle";
 
 export default function Header(){
     return (
@@ -10,22 +11,46 @@ export default function Header(){
                     <Link to="Home">Brennon Oliveira</Link>
                 </h1>
                 <nav>
-                    <ul>
-                        <li>
-                            <Link activeClass="active" to="Home" spy={true} smooth={true} offset={-30} duration={500} >Home</Link>
-                        </li>
-                        <li>
-                            <Link activeClass="active" to="About" spy={true} smooth={true} offset={-30} duration={500}>Sobre</Link>
-                        </li>
-                        <li>
-                            <Link activeClass="active" to="Services" spy={true} smooth={true} offset={-30} duration={500}>Serviços</Link>
-                        </li>
-                        <li>
-                            <Link activeClass="active" to="Contact" spy={true} smooth={true} offset={-30} duration={500}>Contato</Link>
-                        </li>
-                    </ul>
+                    <ul className={style.desktop}>
+                            <li>
+                                <Link activeClass="active" to="Home" spy={true} smooth={true} offset={-30} duration={500} >Home</Link>
+                            </li>
+                            <li>
+                                <Link activeClass="active" to="About" spy={true} smooth={true} offset={-30} duration={500}>Sobre</Link>
+                            </li>
+                            <li>
+                                <Link activeClass="active" to="Services" spy={true} smooth={true} offset={-30} duration={500}>Serviços</Link>
+                            </li>
+                            <li>
+                                <Link activeClass="active" to="Contact" spy={true} smooth={true} offset={-30} duration={500}>Contato</Link>
+                            </li>
+                        </ul>
+                    <SlideToggle
+                    render={({ toggle, setCollapsibleElement }) => (
+                        <div className={style.mobile}>
+                        <i class="fas fa-bars" id="menuMobile" aria-hidden="true" onClick={toggle}></i>
+                            <ul ref={setCollapsibleElement}>
+                                <li>
+                                    <Link activeClass="active" to="Home" spy={true} smooth={true} offset={-30} duration={500} >Home</Link>
+                                </li>
+                                <li>
+                                    <Link activeClass="active" to="About" spy={true} smooth={true} offset={-30} duration={500}>Sobre</Link>
+                                </li>
+                                <li>
+                                    <Link activeClass="active" to="Services" spy={true} smooth={true} offset={-30} duration={500}>Serviços</Link>
+                                </li>
+                                <li>
+                                    <Link activeClass="active" to="Contact" spy={true} smooth={true} offset={-30} duration={500}>Contato</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+                />
+                    
+                    
                 </nav>
             </Element>
+            
         </header>
     )
 }
