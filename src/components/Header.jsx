c0cd4af78c0503bebdbd54612f97e6066706c1d8
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2021-05-12 17:34:44 -0300

    Menu mobile refeito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,53 @@
+import { useEffect, useState } from 'react';
 import { Link, Element} from 'react-scroll';
 import style from './../styles/components/Header.module.css';
-import SlideToggle from "react-slide-toggle";
 
 export default function Header(){
+
+    const [ariaExpanded, setAriaExpanded] = useState(false);
+    const [ariaLabel, setAriaLabel] = useState("Abrir menu");
+
+    function toggleMenu(e){
+        if(e.type === 'touchstart') e.preventDefault();
+        var menu = document.querySelector('.'+style.menu);
+        menu.classList.toggle('active');
+        var active = menu.classList.contains('active')
+        setAriaExpanded(active);
+        setAriaLabel(active?'Abrir menu':'Fechar menu');
+    }
+    
+    useEffect(()=>{
+        var btn = document.querySelector('.'+style.menuButton)
+        btn.addEventListener('touchstart',(e)=>{toggleMenu(e)})
+        btn.addEventListener('click',(e)=>{toggleMenu(e)})
+    },[])
+
     return (
         <header id={style.header}>
-            <Element name="Home" className="container">
+            <div name="Home" className="container">
                 <h1>
                     <Link activeClass="active" to="Home" spy={true} smooth={true} offset={-30} duration={500}>Brennon Oliveira</Link>
                 </h1>
-                <nav>
-                    <ul className={style.desktop}>
-                            <li>
-                                <Link activeClass="active" to="Home" spy={true} smooth={true} offset={-30} duration={500} >Home</Link>
-                            </li>
-                            <li>
-                                <Link activeClass="active" to="About" spy={true} smooth={true} offset={-30} duration={500}>Sobre</Link>
-                            </li>
-                            <li>
-                                <Link activeClass="active" to="Services" spy={true} smooth={true} offset={-30} duration={500}>Serviços</Link>
-                            </li>
-                            <li>
-                                <Link activeClass="active" to="Contact" spy={true} smooth={true} offset={-30} duration={500}>Contato</Link>
-                            </li>
-                        </ul>
-                    <SlideToggle
-                    collapsed
-                    duration={400}
-                    noDisplayStyle
-                    render={({ toggle, setCollapsibleElement }) => (
-                        <div className={style.mobile}>
-                        <i className="fas fa-bars" id="menuMobile" aria-hidden="true" onClick={toggle}></i>
-                            <ul ref={setCollapsibleElement}>
-                                <li>
-                                    <Link activeClass="active" to="Home" spy={true} smooth={true} offset={-30} duration={500} >Home</Link>
-                                </li>
-                                <li>
-                                    <Link activeClass="active" to="About" spy={true} smooth={true} offset={-30} duration={500}>Sobre</Link>
-                                </li>
-                                <li>
-                                    <Link activeClass="active" to="Services" spy={true} smooth={true} offset={-30} duration={500}>Serviços</Link>
-                                </li>
-                                <li>
-                                    <Link activeClass="active" to="Contact" spy={true} smooth={true} offset={-30} duration={500}>Contato</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    )}
-                />
-                    
-                    
+                <nav className={style.menu}>
+                    <ul id='menu' role='menu'>
+                        <li>
+                            <Link activeClass="active" to="Home" spy={true} smooth={true} offset={-30} duration={500} >Home</Link>
+                        </li>
+                        <li>
+                            <Link activeClass="active" to="About" spy={true} smooth={true} offset={-30} duration={500}>Sobre</Link>
+                        </li>
+                        <li>
+                            <Link activeClass="active" to="Services" spy={true} smooth={true} offset={-30} duration={500}>Serviços</Link>
+                        </li>
+                        <li>
+                            <Link activeClass="active" to="Contact" spy={true} smooth={true} offset={-30} duration={500}>Contato</Link>
+                        </li>
+                    </ul>
+                    <button aria-label={ariaLabel} aria-haspopup="true" aria-controls='menu' aria-expanded={ariaExpanded} id='btnMenu' class={style.menuButton}>
+                        <span></span>
+                    </button>
                 </nav>
-            </Element>
+            </div>
             
         </header>
     )
