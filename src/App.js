cf40b063d8635138012d340830a09c3d1a4496cb
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2021-05-14 20:48:48 -0300

    Menu de rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Home from './pages/Home';
+import Resume from './pages/Resume';
+import Projects from './pages/Projects';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 import './App.css';
+import Menu from './components/Menu';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
 
@@ -23,14 +31,27 @@ function App() {
   })
 
   return (
-    <div className="App" onScroll={scroll()}>
-      <Header />
-      <Home />
-      <Footer />
-      {
-        scrollTop&&<Link  activeClass="active" to="Home" spy={true} smooth={true} offset={-30} duration={500} className='upLink'><i class="fas fa-arrow-up"></i></Link>
-      }
-    </div>
+    <Router>
+      <div className="App" onScroll={scroll()}>
+        <Header />
+        <Switch>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+        {
+          scrollTop&&<Link  activeClass="active" to="Home" spy={true} smooth={true} offset={-30} duration={500} className='upLink'><i class="fas fa-arrow-up"></i></Link>
+        }
+        <Menu />
+      </div>
+    </Router>
   );
 }
 
