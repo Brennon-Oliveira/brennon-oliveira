876608b668328f0456c08c62c04ae601a7711c90
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2021-05-15 21:55:55 -0300

    Page Resume concluida

--- src/components/Resume/ResumeInfo.jsx
+++ src/components/Resume/ResumeInfo.jsx
@@ -1,33 +1,40 @@
 import React from 'react';
 import { Element } from 'react-scroll';
 import brennon from '../../images/brennon.jpg';
+import style from './../../styles/components/resume/ResumeInfo.module.css';
+import PDF from './../../assets/curriculo.pdf';
 
 export default function ResumeInfo() {
 
     return(
-        <section id="resumeInfo">
+        <section id={style.resumeInfo}>
             <Element className="container" name="ResumeInfo">
                 <h2>Currículo</h2>
-                <div className="infos">
-                    <header className="profile">
-                        <div className="content">
+                <div className={style.infos}>
+                    <header className={style.profile}>
+                        <div className={style.content}>
                             <h2>Brennon Gabriel de Oliveira</h2>
                             <p>Brasileiro, solteiro, 17 anos</p>
                             <p>E-Mail: brennonoliveira20014@gmail.com</p>
                             <p>Telefone: (42) 99901-7838</p>
                         </div>
-                        <div className="image"><img src={brennon} alt="Brennon Gabriel de Oliveira"/></div>
+                        <div className={style.image}><img src={brennon} alt="Brennon Gabriel de Oliveira"/></div>
                     </header>
-                    <div className="body">
+                    <div className={style.body}>
 
-                        <div className="wrapper">
+                    <div className={style.wrapper}>
+                            <h3>Objetivos</h3>
+                            <p>Atuar como Desenvolvedor Web</p>
+                        </div>
+
+                        <div className={style.wrapper}>
                             <h3>Formação</h3>
                             <ul>
                                 <li>Ensino Médio. Padre Pedro Grzelczak, previsão de conclusão em 2021.</li>
                             </ul>
                         </div>
 
-                        <div className="wrapper">
+                        <div className={style.wrapper}>
                             <h3>Experiência Profissional</h3>
                             <ul>
                                 <li>2021-2021 - Autônomo
@@ -37,7 +44,7 @@ export default function ResumeInfo() {
                             </ul>
                         </div>
 
-                        <div className="wrapper">
+                        <div className={style.wrapper}>
                             <h3>Qualificações e Atividades Complementares</h3>
                             <ul>
                                 <li>Curso Webmaster Front-End Completo (Danki Code, 2020 - Carga Horária: 80 horas)</li>
@@ -48,12 +55,16 @@ export default function ResumeInfo() {
                             </ul>
                         </div>
 
-                        <div className="wrapper">
+                        <div className={style.wrapper}>
                             <h3>Informações Adicionais</h3>
                             <ul>
                                 <li>Conhecimento em Inglês</li>
                                 <li>Conhecimento em Windows e Linux</li>
                             </ul>
+                        </div>
+
+                        <div className={style.button}>
+                            <a href={PDF} download="Currículo.pdf">Baixar em PDF</a>
                         </div>
 
                     </div>
