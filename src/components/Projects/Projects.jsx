d3c750624c7439e551ec4ada3de6da4ec9f02c9a
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2021-05-16 03:00:57 -0300

    Criaçao dos projetos pelos robos

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Element } from 'react-scroll';
 import style from './../../styles/components/projects/MyProjects.module.css';
-import brennon from '../../images/brennon.jpg';
-import banner from '../../images/banner.jpg';
+import loading from '../../images/loading.gif';
 
 export default function MyProjects(){
+
+    const [projects, setProjects] = useState(null);
+    const [images, setImages] = useState(null);
+
+    useEffect(()=>{
+        fetch('https://brennonaleatorioapi.herokuapp.com/projects').
+        then(res => res.json())
+        .then(
+            (result) => {
+              setProjects(result);
+            },
+            (error) => {
+              console.log(error)
+            }
+        )
+    },[])
+
+    if(!projects){
+        return (
+            <div className={style.loading}>
+                <div>
+                    <img src={loading} alt="Loadig" />
+                    <p>Carregando: Pode demorar!</p>
+                </div>
+            </div>
+        );
+    }
 
     return(
         <section id={style.myProjects}>
             <Element name="MyProjects" className="container">
                 <h2>Projetos</h2>
                 <div className={style.projects}>
-
-                    {/* <div className={style.wrapper}>
-                        <img src={brennon} alt="" />
-                        <div className={style.body}>
-                            <h3>Dia da Mulheres</h3>
-                            <p>Dedicado ao um dia tão especial - Feliz dia das mulheres</p>
-                        </div>
-                        <a href='#'>Acessar Site</a>
-                    </div> */}
+                    {
+                        projects.map((val, i)=>{
+                            return <div className={style.wrapper}>
+                                <img src={'https://brennonaleatorioapi.herokuapp.com/images/'+val.image} alt="" />
+                                
+                                <div className={style.body}>
+                                    <h3>{val.title}</h3>
+                                    <p>{val.description}</p>
+                                </div>
+                                <a target="_blank" href={val.url}>Acessar Site</a>
+                            </div>
+                        })
+                    }
 
                 </div>
             </Element>
