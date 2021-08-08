b06470b963a34225ac0de398cbff3cb4509c30a6
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2021-08-08 17:46:34 -0300

    Banner compleot

--- src/sections/home/Banner.jsx
+++ src/sections/home/Banner.jsx
@@ -1,10 +1,106 @@
 import Container from "../../components/Container";
 import style from "../../styles/sections/Banner.module.scss";
 import Link from "../../components/Link";
+import { useEffect } from "react";
 
 export default function Banner(props) {
+    function toggleMenu(e) {
+        if (e.type === "touchstart") e.preventDefault();
+        let menu = document.querySelector("." + style.menu);
+        let btn = document.querySelector("#" + style.header + " button");
+        let body = document.querySelector("body");
+        menu.classList.toggle("active");
+        btn.classList.toggle("active");
+        body.classList.toggle("menu");
+    }
+
+    useEffect(() => {
+        var btn = document.querySelector("#" + style.header + " button");
+        btn.addEventListener("touchstart", (e) => {
+            toggleMenu(e);
+        });
+        btn.addEventListener("click", (e) => {
+            toggleMenu(e);
+        });
+    }, []);
+
     return (
         <section id={style.banner}>
+            <div className={`${style.menu} active`}>
+                <h2>Menu</h2>
+                <nav className={style.options}>
+                    <ul>
+                        <li>
+                            <a href="">
+                                Ferramentas
+                                <div></div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Produtos
+                                <div></div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Especialidades
+                                <div></div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Currículo
+                                <div></div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Projetos
+                                <div></div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Contato<div></div>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <p>
+                    Caso precise de alguma informação, por favor entre em{" "}
+                    <a href="">contato</a> na sessão de contato!
+                </p>
+                <nav className={style.menuSocial}>
+                    <ul>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-whatsapp"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-linkedin-in"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-github"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
             <div className={style.overlay}></div>
             <Container>
                 <header id={style.header}>
@@ -14,7 +110,7 @@ export default function Banner(props) {
                             <div></div>
                         </a>
                     </h1>
-                    <button>
+                    <button className={style.menuButton}>
                         <span></span>
                     </button>
                 </header>
